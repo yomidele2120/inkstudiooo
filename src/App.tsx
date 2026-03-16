53c51897febe80d4150c9959bd52b7067410069f
Yomidele <yomidele2120@gmail.com>
2026-03-16 02:51:35 -0700

    Your message

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import TopicsPage from "./pages/TopicsPage";
 import ComparePage from "./pages/ComparePage";
 import UnderstandingPage from "./pages/UnderstandingPage";
 import ScriptureReaderPage from "./pages/ScriptureReaderPage";
+import QueryDetailPage from "./pages/QueryDetailPage";
+import TopicDetailPage from "./pages/TopicDetailPage";
 import NotFound from "./pages/NotFound";
 import AdminImport from "./pages/AdminImport";
 
@@ -47,11 +49,18 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/results" element={<SearchResultsPage />} />
+          <Route path="/query/:queryId" element={<QueryDetailPage />} />
+          <Route path="/topic/:topicSlug" element={<TopicDetailPage />} />
           <Route path="/topics" element={<TopicsPage />} />
           <Route path="/topics/:topicId" element={<TopicsPage />} />
           <Route path="/compare" element={<ComparePage />} />
           <Route path="/understanding" element={<UnderstandingPage />} />
           <Route path="/read/:type" element={<ScriptureReaderPage />} />
+          <Route path="/read/:type/:bookSlug" element={<ScriptureReaderPage />} />
+          <Route path="/read/:type/:bookSlug/:chapter" element={<ScriptureReaderPage />} />
+          <Route path="/scripture/:type" element={<ScriptureReaderPage />} />
+          <Route path="/scripture/:type/:bookSlug" element={<ScriptureReaderPage />} />
+          <Route path="/scripture/:type/:bookSlug/:chapter" element={<ScriptureReaderPage />} />
           <Route path="/admin/import" element={<AdminImport />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
